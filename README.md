ab62be925a76896e815469f7a563b7cd19195c30
JamieTelos <136978630+JamieTelos@users.noreply.github.com>
2023-06-18 22:10:27 +0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://jamietelos.github.io/telosresourcepack/](https://jamietelos.github.io/telosresourcepack/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/jamietelos/telosresourcepack** and import
+
+## Edit this project ![Build status badge](https://github.com/jamietelos/telosresourcepack/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/jamietelos/telosresourcepack** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/jamietelos/telosresourcepack/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,227 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image9":
+            case "myImage6":return img`
+. . . . . . . . . . . . . . . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image6":
+            case "empty_totem":return img`
+. . . . . . . . . . . . . . . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image1":
+            case "blue_totem":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 8 8 8 8 8 8 8 8 8 8 . . . 
+. . 8 8 f f f f f f f f 8 8 . . 
+. . 8 8 f f f f f f f f 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "wood_totem":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . d d d d d d d d d d . . . 
+. . d d f f f f f f f f d d . . 
+. . d d f f f f f f f f d d . . 
+. . d d d d d d d d d d d d . . 
+. . d d d d d d d d d d d d . . 
+. . d d d d d d d d d d d d . . 
+. . d d d d d d d d d d d d . . 
+. . . d d d d d d d d d d . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "yellow_totem":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . 5 5 f f f f f f f f 5 5 . . 
+. . 5 5 f f f f f f f f 5 5 . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "full_stack":return img`
+. . . . . . . . . . . . . . . . 
+. . e e . . . e e . . . e e . . 
+. 1 8 8 8 . 1 d d d . 1 5 5 5 . 
+. 8 8 8 8 . d d d d . 5 5 5 5 . 
+. 8 8 8 1 . d d d 1 . 5 5 5 1 . 
+. 8 8 8 8 . d d d d . 5 5 5 5 . 
+. 1 8 8 8 . 1 d d d . 1 5 5 5 . 
+. 8 8 8 8 . d d d d . 5 5 5 5 . 
+. 8 8 8 1 . d d d 1 . 5 5 5 1 . 
+. 8 8 8 8 . d d d d . 5 5 5 5 . 
+. 1 8 8 8 . 1 d d d . 1 5 5 5 . 
+. 8 8 8 8 . d d d d . 5 5 5 5 . 
+. 8 8 8 1 . d d d 1 . 5 5 5 1 . 
+. 8 8 8 8 . d d d d . 5 5 5 5 . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image5":
+            case "one_blue":return img`
+. . . . . . . . . . . . . . . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image7":
+            case "two_blue":return img`
+. . . . . . . . . . . . . . . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image8":
+            case "three_blue":return img`
+. . . . . . . . . . . . . . . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image10":
+            case "four_blue":return img`
+. . . . . . . . . . . . . . . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. . e e . . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image11":
+            case "five_blue":return img`
+. . . . . . . . . . . . . . . . 
+. . e e . . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 1 8 8 8 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+. 8 8 8 1 . . e e . . . e e . . 
+. 8 8 8 8 . . e e . . . e e . . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
